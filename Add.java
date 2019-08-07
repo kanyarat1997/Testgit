8623ac5e20fbd1ef4a23ddcc94f3b0cfd201429f
tontrakant <tontrakant.kac@msu.ac.th>
2019-08-07 11:41:21 +0700

    add print2

--- Add.java
+++ Add.java
@@ -1,6 +1,7 @@
 public class Add {
     public Double add(double number1, double number2){
-    	System.out.print('dsd')
+    	System.out.print('dsd');
+    	System.out.print('EEEE');
         return number1+number2+10;
     }
 }
